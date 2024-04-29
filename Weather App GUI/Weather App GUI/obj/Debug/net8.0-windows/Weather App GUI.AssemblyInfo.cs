205921ac2e970d006bf22c3a6646c237f4f08074
Ty the Smonk <pnkbuzz877@gmail.com>
2024-04-29 17:31:29 -0400

    .vs removed

--- Weather App GUI/Weather App GUI/obj/Debug/net8.0-windows/Weather App GUI.AssemblyInfo.cs
+++ Weather App GUI/Weather App GUI/obj/Debug/net8.0-windows/Weather App GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Weather App GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913b59e5e6125c5f720b47ca1c03f859cb81d8de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Weather App GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Weather App GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
